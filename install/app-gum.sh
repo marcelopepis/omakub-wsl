44324a8f3d0044ca010b04eeeb17858a38e3a5f1
Marcelo Pepis <marcelo.pepis@gmail.com>
2025-01-19 20:50:37 -0300

    Update app-gum.sh

--- install/app-gum.sh
+++ install/app-gum.sh
@@ -1,7 +1,3 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
-GUM_VERSION="0.14.1" # Use known good version
-wget -O gum.deb "https://github.com/charmbracelet/gum/releases/download/v0.14.1/gum_0.14.1_amd64.deb"
-sudo apt install -y ./gum.deb
-rm gum.deb
 cd -
